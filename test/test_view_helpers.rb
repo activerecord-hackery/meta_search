52711957d5ab1caf8badd6dcf1e90aef5bd82a38
Ernie Miller <ernie@metautonomo.us>
2010-03-13 16:04:18 -0500

    Additional test coverage

--- test/test_view_helpers.rb
+++ test/test_view_helpers.rb
@@ -47,4 +47,103 @@ class TestViewHelpers < ActionView::TestCase
                        html
     end
   end
+  
+  context "A form using check_boxes with three choices" do
+    setup do
+      @s = Company.search
+      fields_for :search, @s do |f|
+        @f = f
+      end
+    end
+    
+    should "generate the expected HTML without a block" do
+      assert_dom_equal '<input id="search_id_in_1" name="search[id_in][]" ' +
+                       'type="checkbox" value="1" /><label for="search_id_in_1">One</label>' +
+                       '<input id="search_id_in_2" name="search[id_in][]" ' +
+                       'type="checkbox" value="2" /><label for="search_id_in_2">Two</label>' +
+                       '<input id="search_id_in_3" name="search[id_in][]" ' +
+                       'type="checkbox" value="3" /><label for="search_id_in_3">Three</label>',
+                       @f.check_boxes(:id_in, [['One', 1], ['Two', 2], ['Three', 3]])
+    end
+    
+    should "generate the expected HTML with a block" do
+      @f.check_boxes(:id_in, [['One', 1], ['Two', 2], ['Three', 3]]) do |c|
+        concat render :to => :string, :inline => "<p><%= c[:label] %> <%= c[:check_box] %></p>", :locals => {:c => c}
+      end
+      assert_dom_equal output_buffer,
+                      '<p><label for="search_id_in_1">One</label> ' +
+                      '<input id="search_id_in_1" name="search[id_in][]" ' +
+                      'type="checkbox" value="1" /></p>' +
+                      '<p><label for="search_id_in_2">Two</label> ' +
+                      '<input id="search_id_in_2" name="search[id_in][]" ' +
+                      'type="checkbox" value="2" /></p>' +
+                      '<p><label for="search_id_in_3">Three</label> ' +
+                      '<input id="search_id_in_3" name="search[id_in][]" ' +
+                      'type="checkbox" value="3" /></p>'
+    end
+  end
+  
+  context "A form using check_boxes with three choices and a previous selection" do
+    setup do
+      @s = Company.search
+      @s.id_in = [1, 3]
+      fields_for :search, @s do |f|
+        @f = f
+      end
+    end
+    
+    should "generate the expected HTML without a block" do
+      assert_dom_equal '<input checked="checked" id="search_id_in_1" name="search[id_in][]" ' +
+                       'type="checkbox" value="1" /><label for="search_id_in_1">One</label>' +
+                       '<input id="search_id_in_2" name="search[id_in][]" ' +
+                       'type="checkbox" value="2" /><label for="search_id_in_2">Two</label>' +
+                       '<input checked="checked" id="search_id_in_3" name="search[id_in][]" ' +
+                       'type="checkbox" value="3" /><label for="search_id_in_3">Three</label>',
+                       @f.check_boxes(:id_in, [['One', 1], ['Two', 2], ['Three', 3]])
+    end
+    
+    should "generate the expected HTML with a block" do
+      @f.check_boxes(:id_in, [['One', 1], ['Two', 2], ['Three', 3]]) do |c|
+        concat render :to => :string, :inline => "<p><%= c[:label] %> <%= c[:check_box] %></p>", :locals => {:c => c}
+      end
+      assert_dom_equal output_buffer,
+                       '<p><label for="search_id_in_1">One</label> <input checked="checked" id="search_id_in_1" ' +
+                       'name="search[id_in][]" type="checkbox" value="1" /></p><p><label for="search_id_in_2">' +
+                       'Two</label> <input id="search_id_in_2" name="search[id_in][]" type="checkbox" value="2" />' +
+                       '</p><p><label for="search_id_in_3">Three</label> <input checked="checked" id="search_id_in_3" ' +
+                       'name="search[id_in][]" type="checkbox" value="3" /></p>'
+    end
+    
+    context "A form using collection_check_boxes with companies" do
+      setup do
+        @s = Company.search
+        fields_for :search, @s do |f|
+          @f = f
+        end
+      end
+      
+      should "generate the expected HTML without a block" do
+        assert_dom_equal '<input id="search_id_in_1" name="search[id_in][]" type="checkbox" ' +
+                         'value="1" /><label for="search_id_in_1">Initech</label>' +
+                         '<input id="search_id_in_2" name="search[id_in][]" type="checkbox" ' +
+                         'value="2" /><label for="search_id_in_2">Advanced Optical Solutions</label>' +
+                         '<input id="search_id_in_3" name="search[id_in][]" type="checkbox" ' +
+                         'value="3" /><label for="search_id_in_3">Mission Data</label>',
+                         @f.collection_check_boxes(:id_in, Company.all, :id, :name)
+      end
+      
+      should "generate the expected HTML with a block" do
+        @f.collection_check_boxes(:id_in, Company.all, :id, :name) do |c|
+          concat render :to => :string, :inline => "<p><%= c[:label] %> <%= c[:check_box] %></p>", :locals => {:c => c}
+        end
+        assert_dom_equal output_buffer,
+                         '<p><label for="search_id_in_1">Initech</label> ' +
+                         '<input id="search_id_in_1" name="search[id_in][]" type="checkbox" value="1" /></p>' +
+                         '<p><label for="search_id_in_2">Advanced Optical Solutions</label> ' +
+                         '<input id="search_id_in_2" name="search[id_in][]" type="checkbox" value="2" /></p>' +
+                         '<p><label for="search_id_in_3">Mission Data</label> ' +
+                         '<input id="search_id_in_3" name="search[id_in][]" type="checkbox" value="3" /></p>'
+      end
+    end
+  end
 end
