1d2a52e4b58d822702e37e2a75172add60c99661
Ernie Miller <ernie@metautonomo.us>
2010-07-26 10:11:55 -0400

    Properly swap out MetaSearch::Builder for its base in apply_form_for_options!, even when in a nested or namespaced form_for.  (form_for [:admin, @search] against an Article base will now point the form tag at admin_articles_path when no :url option is supplied)

--- lib/meta_search/helpers/form_helper.rb
+++ lib/meta_search/helpers/form_helper.rb
@@ -2,15 +2,14 @@ module MetaSearch
   module Helpers
     module FormHelper
       def apply_form_for_options!(object_or_array, options)
-        if object_or_array.is_a?(Array) && object_or_array.first.is_a?(MetaSearch::Builder)
-          builder = object_or_array.first
+        if object_or_array.is_a?(Array) && (builder = object_or_array.detect {|o| o.is_a? MetaSearch::Builder})
           html_options = {
             :class  => options[:as] ? "#{options[:as]}_search" : "#{builder.base.to_s.underscore}_search",
             :id => options[:as] ? "#{options[:as]}_search" : "#{builder.base.to_s.underscore}_search",
             :method => :get }
           options[:html] ||= {}
           options[:html].reverse_merge!(html_options)
-          options[:url] ||= polymorphic_path(builder.base)
+          options[:url] ||= polymorphic_path(object_or_array.map {|o| o.is_a?(MetaSearch::Builder) ? o.base : o})
         else
           super
         end
