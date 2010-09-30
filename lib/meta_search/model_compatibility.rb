8b59289118c08ff15f2430e2787f936f88cb9fac
Ernie Miller <ernie@metautonomo.us>
2010-09-30 19:54:23 -0400

    Updates to work with pending Rails core patch for instance-level model name

--- lib/meta_search/model_compatibility.rb
+++ lib/meta_search/model_compatibility.rb
@@ -1,52 +1,42 @@
 module MetaSearch
-  # Just a little module to mix in so that ActionPack doesn't complain.
+
   module ModelCompatibility
+    def persisted?
+      false
+    end
+
+    def to_key
+      nil
+    end
+
+    def to_param
+      nil
+    end
 
     def to_model
-      @_compatible_model = CompatibleModel.new(base)
+      self
     end
 
-    class CompatibleModel
-      attr_reader :base
-
-      def initialize(base)
-        @base = base
-      end
-
-      def class
-        @base
-      end
-
-      # Force default "Update search" text
-      def persisted?
-        true
-      end
-
-      def to_key
-        nil
-      end
-
-      def to_param
-        nil
-      end
+    def model_name
+      @_model_name ||= Name.new(self.base)
     end
-
-    class Name < String
-      attr_reader :singular, :plural, :element, :collection, :partial_path, :human, :param_key, :route_key
-      alias_method :cache_key, :collection
-
-      def initialize(base)
-        super("Search")
-        @singular = "search".freeze
-        @plural = "searches".freeze
-        @element = "search".freeze
-        @human = "Search".freeze
-        @collection = "meta_search/searches".freeze
-        @partial_path = "#{@collection}/#{@element}".freeze
-        @param_key = "search".freeze
-        @route_key = base.route_key
-      end
-    end
-
   end
+
+  class Name < String
+    attr_reader :singular, :plural, :element, :collection, :partial_path, :human, :param_key, :route_key
+    alias_method :cache_key, :collection
+
+    def initialize(base)
+      super("Search")
+      @singular = "search".freeze
+      @plural = "searches".freeze
+      @element = "search".freeze
+      @human = "Search".freeze
+      @collection = "meta_search/searches".freeze
+      @partial_path = "#{@collection}/#{@element}".freeze
+      @param_key = "search".freeze
+      @route_key = base.model_name.route_key
+    end
+  end
+
 end
--- meta_search.gemspec
+++ meta_search.gemspec
@@ -92,22 +92,22 @@ you're feeling especially appreciative. It'd help me justify this
 
     if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
       s.add_development_dependency(%q<shoulda>, [">= 0"])
-      s.add_runtime_dependency(%q<activerecord>, ["~> 3.0.0"])
-      s.add_runtime_dependency(%q<activesupport>, ["~> 3.0.0"])
-      s.add_runtime_dependency(%q<actionpack>, ["~> 3.0.0"])
+      s.add_runtime_dependency(%q<activerecord>, ["~> 3.0"])
+      s.add_runtime_dependency(%q<activesupport>, ["~> 3.0"])
+      s.add_runtime_dependency(%q<actionpack>, ["~> 3.0"])
       s.add_runtime_dependency(%q<arel>, ["~> 2.0.0"])
     else
       s.add_dependency(%q<shoulda>, [">= 0"])
-      s.add_dependency(%q<activerecord>, ["~> 3.0.0"])
-      s.add_dependency(%q<activesupport>, ["~> 3.0.0"])
-      s.add_dependency(%q<actionpack>, ["~> 3.0.0"])
+      s.add_dependency(%q<activerecord>, ["~> 3.0"])
+      s.add_dependency(%q<activesupport>, ["~> 3.0"])
+      s.add_dependency(%q<actionpack>, ["~> 3.0"])
       s.add_dependency(%q<arel>, ["~> 2.0.0"])
     end
   else
     s.add_dependency(%q<shoulda>, [">= 0"])
-    s.add_dependency(%q<activerecord>, ["~> 3.0.0"])
-    s.add_dependency(%q<activesupport>, ["~> 3.0.0"])
-    s.add_dependency(%q<actionpack>, ["~> 3.0.0"])
+    s.add_dependency(%q<activerecord>, ["~> 3.0"])
+    s.add_dependency(%q<activesupport>, ["~> 3.0"])
+    s.add_dependency(%q<actionpack>, ["~> 3.0"])
     s.add_dependency(%q<arel>, ["~> 2.0.0"])
   end
 end
