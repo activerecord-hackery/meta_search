12b79592854a72a691b698ea140dfbda35934b3b
Ernie Miller <ernie@metautonomo.us>
2010-09-30 11:45:16 -0400

    Update changelog

--- lib/meta_search/model_compatibility.rb
+++ lib/meta_search/model_compatibility.rb
@@ -1,32 +1,41 @@
 module MetaSearch
   # Just a little module to mix in so that ActionPack doesn't complain.
   module ModelCompatibility
-    def self.included(base)
-      base.extend ClassMethods
-    end
-
-    # Force default "Update search" text
-    def persisted?
-      true
-    end
-
-    def to_key
-      nil
-    end
-
-    def to_param
-      nil
-    end
 
     def to_model
-      self
+      @_compatible_model = CompatibleModel.new(base)
+    end
+
+    class CompatibleModel
+      attr_reader :base
+
+      def initialize(base)
+        @base = base
+      end
+
+      def class
+        @base
+      end
+
+      # Force default "Update search" text
+      def persisted?
+        true
+      end
+
+      def to_key
+        nil
+      end
+
+      def to_param
+        nil
+      end
     end
 
     class Name < String
-      attr_reader :singular, :plural, :element, :collection, :partial_path, :human
+      attr_reader :singular, :plural, :element, :collection, :partial_path, :human, :param_key, :route_key
       alias_method :cache_key, :collection
 
-      def initialize
+      def initialize(base)
         super("Search")
         @singular = "search".freeze
         @plural = "searches".freeze
@@ -34,13 +43,10 @@ module MetaSearch
         @human = "Search".freeze
         @collection = "meta_search/searches".freeze
         @partial_path = "#{@collection}/#{@element}".freeze
+        @param_key = "search".freeze
+        @route_key = base.route_key
       end
     end
 
-    module ClassMethods
-      def model_name
-        @_model_name ||= Name.new
-      end
-    end
   end
 end
